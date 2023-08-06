8a39c2c7708b042a928cc163aae3c950889bd789
Avishek Raut <avishekraut123@gmail.com>
2023-08-06 10:35:22 +0545

    Added Footer Component

---
+++ traveljournal/src/Components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Made with ❤️ by&nbsp;
+        <a className="website-link" href="https://github.com/Avishekraut">
+          Avishek Raut
+        </a>
+      </p>
+    </footer>
+  );
+}
