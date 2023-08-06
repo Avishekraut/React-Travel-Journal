e2e8c12e9d55f499b214b487483a07280a104a38
Avishek Raut <avishekraut123@gmail.com>
2023-08-06 11:19:49 +0545

    updated image div

--- traveljournal/src/Components/Card.js
+++ traveljournal/src/Components/Card.js
@@ -4,15 +4,19 @@ export default function Card(props) {
   return (
     <>
       <div className="card">
-        <img
-          className="card-img"
-          src={require(`../images/${props.item.imageUrl}`)}
-        ></img>
+        <div className="img-div">
+          <img
+            className="card-img"
+            src={require(`../images/${props.item.imageUrl}`)}
+          ></img>
+        </div>
         <div className="card-details">
           <div className="card-country">
             <img className="location-icon" src={locationicon}></img>
             <p>{props.item.location}</p>
-            <a href={props.item.googleMapsUrl} target="_blank">View on Google Maps</a>
+            <a href={props.item.googleMapsUrl} target="_blank">
+              View on Google Maps
+            </a>
           </div>
           <h1 className="card-title">{props.item.title}</h1>
           <p className="card-date">
