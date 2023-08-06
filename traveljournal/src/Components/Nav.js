3cda87241bc5cbaa6244ff1eb6a8bf7478e894ef
Avishek Raut <avishekraut123@gmail.com>
2023-08-06 10:12:59 +0545

    Added Responsiveness  using Media Queries

--- traveljournal/src/Components/Nav.js
+++ traveljournal/src/Components/Nav.js
@@ -5,7 +5,7 @@ export default function Nav(props) {
     return(
         <nav className= "nav-bar"> 
             <img className= "logo"src={props.img}></img>
-            <p className="nav-title">My Travel Journal</p>
+            <p className="nav-title">my travel journal</p>
         </nav>
     )
 }
