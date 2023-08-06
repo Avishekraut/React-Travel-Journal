2ac8761e1b76bb0b3c91f2444be4ae308116927e
Avishek Raut <avishekraut123@gmail.com>
2023-08-06 11:04:50 +0545

    Added New Data

--- traveljournal/src/data.js
+++ traveljournal/src/data.js
@@ -1,6 +1,26 @@
 const data = [
     {
         id: 1,
+        title: "Pokhara",
+        location: "Nepal",
+        googleMapsUrl: "https://goo.gl/maps/fJVJa8jH8ESQPsHy5",
+        startDate: "15 Dec, 2021",
+        endDate: "18 Jan, 2021",
+        description: "Pokhara is a city on Phewa Lake, in central Nepal. It’s known as a gateway to the Annapurna Circuit, a popular trail in the Himalayas. Tal Barahi Temple, a 2-story pagoda, sits on an island in the lake.",
+        imageUrl: "Phewa-Lake.jpg"
+    },
+    {
+        id: 2,
+        title: "Rara Lake",
+        location: "Nepal",
+        googleMapsUrl: "https://goo.gl/maps/wydcxVS9PY6sePFp6",
+        startDate: "27 May, 2021",
+        endDate: "8 Jun, 2021",
+        description: "Visit Nepal's deepest Lake, the Rara, hiking through lovely Juniper forests and enjoy the spectacular landscape that surrounds this pristine lake.",
+        imageUrl: "rara-lake.jpeg"
+    },
+    {
+        id: 3,
         title: "Mount Fuji",
         location: "Japan",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
@@ -8,16 +28,6 @@ const data = [
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         imageUrl: "mount-fuji.png"
-    },
-    {
-        id: 2,
-        title: "Sydney Opera House",
-        location: "Australia",
-        googleMapsUrl: "https://goo.gl/maps/1WGB6U3uz3MTqrh1A",
-        startDate: "27 May, 2021",
-        endDate: "8 Jun, 2021",
-        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
-        imageUrl: "sydney-opera-house.png"
     },
     {
         id: 3,
@@ -28,6 +38,16 @@ const data = [
         endDate: "18 Nov, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         imageUrl: "geirangerfjord.png"
+    },
+    {
+        id: 5,
+        title: "Manang",
+        location: "Nepal",
+        googleMapsUrl: "https://goo.gl/maps/rPDe3iHn9eT8mggK7",
+        startDate: "15 Dec, 2021",
+        endDate: "18 Jan, 2021",
+        description: "Manang is a classic stop on the Annapurna Circuit Trek, as this Nepali village high in the Himalayas is a perfect place for acclimatization.",
+        imageUrl: "manang.jpg"
     }
 ]
 
